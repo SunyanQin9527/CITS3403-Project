b57b6b079e49868885d05e264d6f6a1b1b564193
LeyiChen62 <167012851+LeyiChen62@users.noreply.github.com>
2024-05-19 11:54:11 +0800

    Delete Agile processes directory

--- Agile processes/Tests/shop.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('shopButton').onclick = function() {
-    window.location.href = 'shop.html'; // Change this to the correct path
-};
-
--- Agile processes/app.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-import os
